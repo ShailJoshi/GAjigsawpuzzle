295c4b78516b5a46bb7a5ec4b0a18bdc9d31951a
ShailJoshi <shailjoshi007@gmail.com>
2017-04-08 14:32:39 +0530

    dissimilarity and fitness calc using LUT

--- fitnesscalc.m
+++
@@ -1,8 +0,0 @@
-function F=fitnesscalc(A,dissLUT,N)
-F=0;
-    for i=1:N
-        for j=1:N
-            F=F + dissLUT(A(i,(j*(j<N)+1)),A(i,j),1)*(j<N) + dissLUT(A(i*(i<N)+1,j),A(i,j),2)*(i<N);            % dissLUT(i,j,1) piece i is right of j and dissLUT(i,j,2) i is below j
-        end
-    end
-end
